7ac1b329ccc2589a226699fde113441959b210e4
masri <dahirma@vcu.edu>
2022-08-04 16:50:24 -0400

    design changed for education.html

--- vueJs/default.js
+++ vueJs/default.js
@@ -1,4 +1,4 @@
-let app = Vue.createApp({
+var app = Vue.createApp({
     data: function(){
         return {
             greeting: 'Hello Vue 3!',
@@ -60,68 +60,452 @@ let app = Vue.createApp({
 
     }
 })
-let app2 = Vue.createApp({
-    data: function(){
-        return {
-            greeting: 'Hello Vue 3!',
-            isVisible: false,
-            isVisible2: true,
-            button_to_activate_box: false,
-            button_text: "show",
-            windowHeight: window.innerHeight,
-            windowWidth: window.innerWidth,
-            rand: '10px',
-            color: 'red',
-            middle: (window.innerWidth - 1180)/2,
-            middle2: (window.innerWidth - 10)/2,
-        }
-    },
-    mounted() {
-        this.$nextTick(() => {
-            window.addEventListener('resize', this.onResize);
-        })
-    },
-
-    beforeDestroy() {
-        window.removeEventListener('resize', this.onResize);
-    },
-    methods:{
-        toggleBox() {
-            this.button_to_activate_box = !this.button_to_activate_box;
-
-            if (this.button_text == "show"){
-                this.button_text = "hide";
-            }
-            else{
-                this.button_text = "show";
-            }
-        },
-        greet(greeting){
-            console.log(greeting);
-        },
-        onResize() {
-            this.windowHeight = window.innerHeight;
-            this.windowWidth = window.innerWidth;
-            this.middle = (window.innerWidth - 1180)/2;
-            this.middle2 = (window.innerWidth - 50)/2;
-        },
-        modelStyle: function(slide) {
-
-            if (slide === 'middle'){
-                return {
-                    'left': `${this.middle}px`
-                };
-            }
-            else if (slide === 'middle2'){
-                return {
-                    'left': `${this.middle2}px`
-                };
-            }
-
-        }
-
-    }
-})
-
 app.mount('#app');
+
+var app2 = Vue.createApp({
+    data: function(){
+        return {
+            greeting: 'Hello Vue 3!',
+            isVisible: false,
+            isVisible2: true,
+            button_to_activate_box: false,
+            button_text: "show",
+            windowHeight: window.innerHeight,
+            windowWidth: window.innerWidth,
+            rand: '10px',
+            color: 'red',
+            middle: (window.innerWidth - 1180)/2,
+            middle2: (window.innerWidth - 10)/2,
+        }
+    },
+    mounted() {
+        this.$nextTick(() => {
+            window.addEventListener('resize', this.onResize);
+        })
+    },
+
+    beforeDestroy() {
+        window.removeEventListener('resize', this.onResize);
+    },
+    methods:{
+        toggleBox() {
+            this.button_to_activate_box = !this.button_to_activate_box;
+
+            if (this.button_text == "show"){
+                this.button_text = "hide";
+            }
+            else{
+                this.button_text = "show";
+            }
+        },
+        greet(greeting){
+            console.log(greeting);
+        },
+        onResize() {
+            this.windowHeight = window.innerHeight;
+            this.windowWidth = window.innerWidth;
+            this.middle = (window.innerWidth - 1180)/2;
+            this.middle2 = (window.innerWidth - 50)/2;
+        },
+        modelStyle: function(slide) {
+
+            if (slide === 'middle'){
+                return {
+                    'left': `${this.middle}px`
+                };
+            }
+            else if (slide === 'middle2'){
+                return {
+                    'left': `${this.middle2}px`
+                };
+            }
+
+        }
+
+    }
+})
 app2.mount('#app2');
+
+var app3 = Vue.createApp({
+    data: function(){
+        return {
+            greeting: 'Hello Vue 3!',
+            isVisible: false,
+            isVisible2: true,
+            button_to_activate_box: false,
+            button_text: "show",
+            windowHeight: window.innerHeight,
+            windowWidth: window.innerWidth,
+            rand: '10px',
+            color: 'red',
+            middle: (window.innerWidth - 1180)/2,
+            middle2: (window.innerWidth - 10)/2,
+        }
+    },
+    mounted() {
+        this.$nextTick(() => {
+            window.addEventListener('resize', this.onResize);
+        })
+    },
+
+    beforeDestroy() {
+        window.removeEventListener('resize', this.onResize);
+    },
+    methods:{
+        toggleBox() {
+            this.button_to_activate_box = !this.button_to_activate_box;
+
+            if (this.button_text == "show"){
+                this.button_text = "hide";
+            }
+            else{
+                this.button_text = "show";
+            }
+        },
+        greet(greeting){
+            console.log(greeting);
+        },
+        onResize() {
+            this.windowHeight = window.innerHeight;
+            this.windowWidth = window.innerWidth;
+            this.middle = (window.innerWidth - 1180)/2;
+            this.middle2 = (window.innerWidth - 50)/2;
+        },
+        modelStyle: function(slide) {
+
+            if (slide === 'middle'){
+                return {
+                    'left': `${this.middle}px`
+                };
+            }
+            else if (slide === 'middle2'){
+                return {
+                    'left': `${this.middle2}px`
+                };
+            }
+
+        }
+
+    }
+})
+app3.mount('#app3');
+
+var app4 = Vue.createApp({
+    data: function(){
+        return {
+            greeting: 'Hello Vue 3!',
+            isVisible: false,
+            isVisible2: true,
+            button_to_activate_box: false,
+            button_text: "show",
+            windowHeight: window.innerHeight,
+            windowWidth: window.innerWidth,
+            rand: '10px',
+            color: 'red',
+            middle: (window.innerWidth - 1180)/2,
+            middle2: (window.innerWidth - 10)/2,
+        }
+    },
+    mounted() {
+        this.$nextTick(() => {
+            window.addEventListener('resize', this.onResize);
+        })
+    },
+
+    beforeDestroy() {
+        window.removeEventListener('resize', this.onResize);
+    },
+    methods:{
+        toggleBox() {
+            this.button_to_activate_box = !this.button_to_activate_box;
+
+            if (this.button_text == "show"){
+                this.button_text = "hide";
+            }
+            else{
+                this.button_text = "show";
+            }
+        },
+        greet(greeting){
+            console.log(greeting);
+        },
+        onResize() {
+            this.windowHeight = window.innerHeight;
+            this.windowWidth = window.innerWidth;
+            this.middle = (window.innerWidth - 1180)/2;
+            this.middle2 = (window.innerWidth - 50)/2;
+        },
+        modelStyle: function(slide) {
+
+            if (slide === 'middle'){
+                return {
+                    'left': `${this.middle}px`
+                };
+            }
+            else if (slide === 'middle2'){
+                return {
+                    'left': `${this.middle2}px`
+                };
+            }
+
+        }
+
+    }
+})
+app4.mount('#app4');
+
+var app5 = Vue.createApp({
+    data: function(){
+        return {
+            greeting: 'Hello Vue 3!',
+            isVisible: false,
+            isVisible2: true,
+            button_to_activate_box: false,
+            button_text: "show",
+            windowHeight: window.innerHeight,
+            windowWidth: window.innerWidth,
+            rand: '10px',
+            color: 'red',
+            middle: (window.innerWidth - 1180)/2,
+            middle2: (window.innerWidth - 10)/2,
+        }
+    },
+    mounted() {
+        this.$nextTick(() => {
+            window.addEventListener('resize', this.onResize);
+        })
+    },
+
+    beforeDestroy() {
+        window.removeEventListener('resize', this.onResize);
+    },
+    methods:{
+        toggleBox() {
+            this.button_to_activate_box = !this.button_to_activate_box;
+
+            if (this.button_text == "show"){
+                this.button_text = "hide";
+            }
+            else{
+                this.button_text = "show";
+            }
+        },
+        greet(greeting){
+            console.log(greeting);
+        },
+        onResize() {
+            this.windowHeight = window.innerHeight;
+            this.windowWidth = window.innerWidth;
+            this.middle = (window.innerWidth - 1180)/2;
+            this.middle2 = (window.innerWidth - 50)/2;
+        },
+        modelStyle: function(slide) {
+
+            if (slide === 'middle'){
+                return {
+                    'left': `${this.middle}px`
+                };
+            }
+            else if (slide === 'middle2'){
+                return {
+                    'left': `${this.middle2}px`
+                };
+            }
+
+        }
+
+    }
+})
+app5.mount('#app5');
+
+var app6 = Vue.createApp({
+    data: function(){
+        return {
+            greeting: 'Hello Vue 3!',
+            isVisible: false,
+            isVisible2: true,
+            button_to_activate_box: false,
+            button_text: "show",
+            windowHeight: window.innerHeight,
+            windowWidth: window.innerWidth,
+            rand: '10px',
+            color: 'red',
+            middle: (window.innerWidth - 1180)/2,
+            middle2: (window.innerWidth - 10)/2,
+        }
+    },
+    mounted() {
+        this.$nextTick(() => {
+            window.addEventListener('resize', this.onResize);
+        })
+    },
+
+    beforeDestroy() {
+        window.removeEventListener('resize', this.onResize);
+    },
+    methods:{
+        toggleBox() {
+            this.button_to_activate_box = !this.button_to_activate_box;
+
+            if (this.button_text == "show"){
+                this.button_text = "hide";
+            }
+            else{
+                this.button_text = "show";
+            }
+        },
+        greet(greeting){
+            console.log(greeting);
+        },
+        onResize() {
+            this.windowHeight = window.innerHeight;
+            this.windowWidth = window.innerWidth;
+            this.middle = (window.innerWidth - 1180)/2;
+            this.middle2 = (window.innerWidth - 50)/2;
+        },
+        modelStyle: function(slide) {
+
+            if (slide === 'middle'){
+                return {
+                    'left': `${this.middle}px`
+                };
+            }
+            else if (slide === 'middle2'){
+                return {
+                    'left': `${this.middle2}px`
+                };
+            }
+
+        }
+
+    }
+})
+app6.mount('#app6');
+
+var app7 = Vue.createApp({
+    data: function(){
+        return {
+            greeting: 'Hello Vue 3!',
+            isVisible: false,
+            isVisible2: true,
+            button_to_activate_box: false,
+            button_text: "show",
+            windowHeight: window.innerHeight,
+            windowWidth: window.innerWidth,
+            rand: '10px',
+            color: 'red',
+            middle: (window.innerWidth - 1180)/2,
+            middle2: (window.innerWidth - 10)/2,
+        }
+    },
+    mounted() {
+        this.$nextTick(() => {
+            window.addEventListener('resize', this.onResize);
+        })
+    },
+
+    beforeDestroy() {
+        window.removeEventListener('resize', this.onResize);
+    },
+    methods:{
+        toggleBox() {
+            this.button_to_activate_box = !this.button_to_activate_box;
+
+            if (this.button_text == "show"){
+                this.button_text = "hide";
+            }
+            else{
+                this.button_text = "show";
+            }
+        },
+        greet(greeting){
+            console.log(greeting);
+        },
+        onResize() {
+            this.windowHeight = window.innerHeight;
+            this.windowWidth = window.innerWidth;
+            this.middle = (window.innerWidth - 1180)/2;
+            this.middle2 = (window.innerWidth - 50)/2;
+        },
+        modelStyle: function(slide) {
+
+            if (slide === 'middle'){
+                return {
+                    'left': `${this.middle}px`
+                };
+            }
+            else if (slide === 'middle2'){
+                return {
+                    'left': `${this.middle2}px`
+                };
+            }
+
+        }
+
+    }
+})
+app7.mount('#app7');
+
+var app8 = Vue.createApp({
+    data: function(){
+        return {
+            greeting: 'Hello Vue 3!',
+            isVisible: false,
+            isVisible2: true,
+            button_to_activate_box: false,
+            button_text: "show",
+            windowHeight: window.innerHeight,
+            windowWidth: window.innerWidth,
+            rand: '10px',
+            color: 'red',
+            middle: (window.innerWidth - 1180)/2,
+            middle2: (window.innerWidth - 10)/2,
+        }
+    },
+    mounted() {
+        this.$nextTick(() => {
+            window.addEventListener('resize', this.onResize);
+        })
+    },
+
+    beforeDestroy() {
+        window.removeEventListener('resize', this.onResize);
+    },
+    methods:{
+        toggleBox() {
+            this.button_to_activate_box = !this.button_to_activate_box;
+
+            if (this.button_text == "show"){
+                this.button_text = "hide";
+            }
+            else{
+                this.button_text = "show";
+            }
+        },
+        greet(greeting){
+            console.log(greeting);
+        },
+        onResize() {
+            this.windowHeight = window.innerHeight;
+            this.windowWidth = window.innerWidth;
+            this.middle = (window.innerWidth - 1180)/2;
+            this.middle2 = (window.innerWidth - 50)/2;
+        },
+        modelStyle: function(slide) {
+
+            if (slide === 'middle'){
+                return {
+                    'left': `${this.middle}px`
+                };
+            }
+            else if (slide === 'middle2'){
+                return {
+                    'left': `${this.middle2}px`
+                };
+            }
+
+        }
+
+    }
+})
+app8.mount('#app8');
