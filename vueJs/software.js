6818ab098f109dc2d4b7a539668faa71a885367c
Masrik Dahir <dahirma@vcu.edu>
2023-01-18 21:49:33 -0500

    map search bar fixed

--- vueJs/software.js
+++ vueJs/software.js
@@ -154,6 +154,18 @@ const app_software = {
 const app_map = {
     data() {
         return {
+            greeting: 'Hello Vue 3!',
+            isVisible: false,
+            isVisible2: true,
+            button_to_activate_box: false,
+            button_text: "show",
+            windowHeight: window.innerHeight,
+            windowWidth: window.innerWidth,
+            rand: '10px',
+            color: 'red',
+            middle: (window.innerWidth - 900)/2,
+            middle2: (window.innerWidth - 10)/2,
+
             searchQuery: null,
             resources:[
                 {
@@ -175,6 +187,50 @@ const app_map = {
             }else{
                 return this.resources;
             }
+        }
+    },
+
+
+
+    mounted() {
+        this.$nextTick(() => {
+            window.addEventListener('resize', this.onResize);
+        })
+    },
+    beforeDestroy() {
+        window.removeEventListener('resize', this.onResize);
+    },
+    methods: {
+        toggleBox() {
+            this.button_to_activate_box = !this.button_to_activate_box;
+
+            if (this.button_text == "show") {
+                this.button_text = "hide";
+            } else {
+                this.button_text = "show";
+            }
+        },
+        greet(greeting) {
+            console.log(greeting);
+        },
+        onResize() {
+            this.windowHeight = window.innerHeight;
+            this.windowWidth = window.innerWidth;
+            this.middle = (window.innerWidth - 1000) / 2;
+            this.middle2 = (window.innerWidth - 50) / 2;
+        },
+        modelStyle: function (slide) {
+
+            if (slide === 'middle') {
+                return {
+                    'left': `${this.middle}px`
+                };
+            } else if (slide === 'middle2') {
+                return {
+                    'left': `${this.middle2}px`
+                };
+            }
+
         }
     }
 }
@@ -228,4 +284,3 @@ const top_nav = {
 
 Vue.createApp(app_software).mount('#app_software')
 Vue.createApp(app_map).mount('#app_map')
-Vue.createApp(app_map).mount('#top_nav')
