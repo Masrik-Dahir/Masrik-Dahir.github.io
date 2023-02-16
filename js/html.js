cdc93f74ac5132d6c2ba260235a4be265a2671ce
Masrik Dahir <dahirma@vcu.edu>
2023-02-16 18:23:55 -0500

    bug fixes x6

--- js/html.js
+++ js/html.js
@@ -1,5 +1,5 @@
 $(function(){
     $("#header").load("header.html");
-    $("#footer").load("footer.html");
+    $("#footer-relative").load("footer.html");
     $("#left_column").load("left_column.html");
 });
