840cf7a4397fb76d42ff76974df5f49d95ed050e
Masrik Dahir <dahirma@vcu.edu>
2024-04-06 19:51:49 -0400

    added UK countries

--- vueJs/software.js
+++ vueJs/software.js
@@ -409,6 +409,109 @@ const app_pic = {
     }
 }
 
+const app_pic_gbr = {
+    data() {
+        return {
+            showingPopupIndex: null, // Index of the currently shown popup
+            searchQuery: null,
+            resources:[
+                {
+                    title:"England",
+                    abv:"GB-ENG",
+                    image:"./Images/England/GB-ENG.svg.png",
+                    url:"",
+                },
+                {
+                    title:"Wales",
+                    abv:"GB-WLS",
+                    image:"./Images/Wales/GB-WLS.svg.png",
+                    url:"",
+                },
+                {
+                    title:"Northern Ireland",
+                    abv:"GB-NIR",
+                    image:"./Images/North Ireland/GB-NIR.svg.png",
+                    url:"",
+                },
+                {
+                    title:"Scotland",
+                    abv:"GB-SCT",
+                    image:"./Images/Scotland/GB-SCT.svg.png",
+                    url:"",
+                },
+                {
+                    title:"Ireland",
+                    abv:"IRL",
+                    image:"./Images/Ireland/IRL.svg.png",
+                    url:"",
+                }
+            ]
+        };
+    },
+    computed: {
+        resultQuery(){
+            if(this.searchQuery){
+                return this.resources.filter((item)=>{
+                    return this.searchQuery.toLowerCase().split(' ').every(v => item.title.toLowerCase().includes(v))
+                })
+            }else{
+                return this.resources;
+            }
+        }
+    },
+
+
+
+    mounted() {
+        this.$nextTick(() => {
+            window.addEventListener('resize', this.onResize);
+        })
+    },
+    beforeDestroy() {
+        window.removeEventListener('resize', this.onResize);
+    },
+    methods: {
+        toggleBox() {
+            this.button_to_activate_box = !this.button_to_activate_box;
+
+            if (this.button_text == "show") {
+                this.button_text = "hide";
+            } else {
+                this.button_text = "show";
+            }
+        },
+        greet(greeting) {
+            console.log(greeting);
+        },
+        onResize() {
+            this.windowHeight = window.innerHeight;
+            this.windowWidth = window.innerWidth;
+            this.middle = (window.innerWidth - 1000) / 2;
+            this.middle2 = (window.innerWidth - 50) / 2;
+        },
+        modelStyle: function (slide) {
+
+            if (slide === 'middle') {
+                return {
+                    'left': `${this.middle}px`
+                };
+            } else if (slide === 'middle2') {
+                return {
+                    'left': `${this.middle2}px`
+                };
+            }
+
+        },
+        openPopup(index) {
+            this.showingPopupIndex = index;
+        },
+        closePopup() {
+            this.showingPopupIndex = null;
+        }
+    }
+}
+
+
 const app_kentucky = {
     data() {
         return {
@@ -1289,6 +1392,7 @@ const top_nav = {
 Vue.createApp(app_software).mount('#app_software')
 Vue.createApp(app_map).mount('#app_map')
 Vue.createApp(app_pic).mount('#app_pic')
+Vue.createApp(app_pic_gbr).mount('#app_pic_gbr')
 Vue.createApp(app_kentucky).mount('#app_kentucky')
 Vue.createApp(app_maine).mount('#app_maine')
 Vue.createApp(app_new_hampshire).mount('#app_new_hampshire')
